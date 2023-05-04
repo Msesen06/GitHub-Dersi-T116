a582e05da24acaef2422bc2cc9c86a867ffdb0bd
Mustafa sesen <sesenm0506@gmail.com>
2023-05-04 15:45:37 +0300

    ilk commit

---
+++ src/day01/C01.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        //C:\Users\HP\Desktop\GithubDersi
+        System.out.println("merhaba");
+
+
+        // git init komutu ile birlikte bir workindirectory i git ile irtibatlandirir
+        // bu komut her proje icin bir kez calıstırılır. bu komutla beraber git icinde bulunulan klasoru kontrol
+        // etmeye baslar
+
+        // bu klasorde her yedek almak istedigimizde commit yapmalıyız.
+        // asagıdaki  3 komut bizim dosyalarımızı yedekler.
+        /*
+        1 . ( git add . ) son kaydedilen versiyondan sonra projede yapılan degisiklikleri belirler
+        2.  ( git commit -m "commit ismi") :  projenin son durumunu bir versiyon olarak kaydeder."" tırnak icinde
+        yazılan commit isminin anlamlı olmasında fayda vardır.
+        ilerleyen asamalarda hangi commite donmek isteyecegimizi acıklamalardan bulabiliriz.
+
+         */
+    }
+}
