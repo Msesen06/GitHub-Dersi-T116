2856946c4c3557ba22f73446bb747cfd625ed939
Mustafa sesen <sesenm0506@gmail.com>
2023-05-04 15:59:20 +0300

    C02 de acıklama eklnedi

--- src/day01/C02.java
+++ src/day01/C02.java
@@ -3,5 +3,6 @@ package day01;
 public class C02 {
     public static void main(String[] args) {
         System.out.println("merhaba 2");
+        // git commit -m ile son degisklikleri  ekler
     }
 }
