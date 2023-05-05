e25ce25304e6b3655aa426aec08d4ec43dda3c61
Mustafa sesen <sesenm0506@gmail.com>
2023-05-05 15:15:21 +0300

    yeni acıklama eklendi

--- src/day01/C03.java
+++ src/day01/C03.java
@@ -3,5 +3,9 @@ package day01;
 public class C03 {
     public static void main(String[] args) {
         System.out.println("yeni class");
+        /*
+        kisisel kullanımda eski bir commite gitmek icin tek bir komut yeterlidir.
+        ancak commit yaparak son halini gondermeliyiz.
+         */
     }
 }
