586d7843b914a5b7f96a039f7ed71cef80966d7c
Mustafa sesen <sesenm0506@gmail.com>
2023-05-05 19:28:10 +0300

    deneme commiti

---
+++ src/day01/branchKullanimi.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class branchKullanimi {
+    public static void main(String[] args) {
+        /*
+        bir proje devam ederken
+        deneme amaclı veya asıl proje uzerinde degisiklik yapmadan
+        branch kullaniriz
+        komutlar :
+
+        git branc : icinde bulunulan brancı gosterir ve listesini
+        git branch ad: ad isminde bir branch olusturur.
+        git checkout adi : o brancha gecer.
+         */
+    }
+}
